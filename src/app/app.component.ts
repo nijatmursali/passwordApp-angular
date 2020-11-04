b1009218ddb782806c182f9f2c383bc63e64a9ae
Nijat Mursali <nijatmursali@gmail.com>
2020-11-04 22:49:30 +0100

    finished basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-angular';
+  title = 'Password Generator';
+  enteredLength: Number = 0;
+  useLetters: Boolean = false;
+  useNumbers: Boolean = false;
+  useSymbols: Boolean = false;
+  password = '';
+
+
+  onchangeLength(value: string) {
+    this.enteredLength = parseInt(value);
+  }
+  onChangeuseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeuseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeuseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClicked() {
+
+    let validChars = '';
+    let generatedPass = '';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvyz';
+    const symbols = '!@#$%^&*()__+-=';
+
+    if(this.useLetters) {
+      validChars += letters;
+    }
+    if(this.useNumbers) {
+      validChars += numbers;
+    }
+    if(this.useSymbols) {
+      validChars += symbols;
+    }
+
+    for (let i = 0; i < this.enteredLength; i++) {
+      const element = Math.floor(Math.random() * validChars.length);
+      generatedPass += validChars[element];
+    }
+
+    this.password = generatedPass;
+
+  }
 }
